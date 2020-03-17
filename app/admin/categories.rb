0c9840239bdc7ef41297cd0f20b6446a90df1887
monika kasetiya <kasetiyamonika@gmail.com>
2020-03-17 12:06:37 +0530

    record add

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,28 +1,16 @@
-ActiveAdmin.register Service do
+ActiveAdmin.register Category do
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-permit_params :name, :price, :description, :image
-
-  form do |f|
-   f.semantic_errors # shows errors on :base
-   f.inputs do
-     # f.select :category
-     f.input :name
-     f.input :price
-     f.input :description
-     f.input :image, as: :file
-     f.actions
-   end
- end
+  permit_params :name
   #
   # or
   #
   # permit_params do
-  #   permitted = [:name, :price, :description]
+  #   permitted = [:name]
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
--- app/admin/products.rb
+++ app/admin/products.rb
@@ -5,7 +5,22 @@ ActiveAdmin.register Product do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :name, :category_id, :description, :mrp, :discount, :discount_type
+  permit_params :name, :category_id, :description, :mrp, :discount, :discount_type, :image
+
+  form do |f|
+   f.semantic_errors # shows errors on :base
+   f.inputs do
+     # f.select :category
+     f.input :name
+     f.input :category, as: :select
+     f.input :description
+     f.input :mrp
+     f.input :discount
+     f.input :discount_type
+     f.input :image, as: :file
+     f.actions
+   end
+ end
   #
   # or
   #
@@ -14,5 +29,5 @@ ActiveAdmin.register Product do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+
 end
